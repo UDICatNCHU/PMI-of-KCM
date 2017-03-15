0bcb62c66c36bd6989a44cffd232fe64569b14d3
davidtnfsh <davidtnfsh@gmail.com>
2017-03-15 10:45:17 +0800

    PMI now will build frequency depends on the same context of KCM

--- PMI.py
+++ PMI.py
@@ -4,48 +4,46 @@ from pymongo import MongoClient
 
 class PMI(object):
     """docstring for PMI"""
-    client = MongoClient()
-    db = client['nlp']
-    Collect = db['pmi']
     def __init__(self, uri=None):
-        PMI.client = MongoClient(uri)
-    # @classmethod
-    # def from_string(cls, date_as_string):
-    #     day, month, year = map(int, date_as_string.split('-'))
-    #     date1 = cls(day, month, year)
-    #     return date1
+        self.client = MongoClient(uri)
+        self.db = self.client['nlp']
+        self.Collect = self.db['pmi']
             
-    @classmethod
-    def search_word_freq(cls, keyword):
-        result = cls.Collect.find({'key':keyword}, {'freq':1, '_id':False}).limit(1)
+    def search_word_freq(self, keyword):
+        result = self.Collect.find({'key':keyword}, {'freq':1, '_id':False}).limit(1)
         if result.count()==0:
             return 1
         return result[0]['freq']
 
-    @classmethod
-    def checkHasPMI(cls, keyword):
-        result = cls.Collect.find({'key':keyword}, {'freq':1, 'value':1, '_id':False}).limit(1)
+    def checkHasPMI(self, keyword):
+        result = self.Collect.find({'key':keyword}, {'freq':1, 'value':1, '_id':False}).limit(1)
         if result.count() == 0:
             return False
         elif list(result)[0]['value'] == []:
             return False
         return True
 
+    def getWordFreqList(self):
+        result = {}
+        for i in self.db['kcm'].find():
+            keyword = i['key']
+            for correlationTermsArr in i['value']:
+                corTermCount = correlationTermsArr[1]
+
+                # accumulate keyword'c frequency.
+                result[keyword] = result.setdefault(keyword, 0) + corTermCount
+
+        return result.items()
+
     def build(self):
         import pymongo
         self.Collect.remove({})
 
-        self.wordfreqfile=open("cht.modelWordlist_countingCombine",'r')
         # read frequency file and insert into MongoDB. 
         # with format {key:'中興大學', freq:100, value:[]}
         result = []
-        for line in self.wordfreqfile:
-            line=line.split()
-            if(len(line)==2):
-                result.append({'key':line[0], 'freq':line[1], 'value':[]})
-                # [line[0]]=int(line[1])
-            else:
-                raise Exception('dd')
+        for keyword, amount in self.getWordFreqList():
+            result.append({'key':keyword, 'freq':amount, 'value':[]})
 
         self.Collect.insert(result)
         self.Collect.create_index([("key", pymongo.HASHED)])
@@ -66,7 +64,6 @@ class PMI(object):
                 # frequency of total keyword = 154451970
                 # p(x, y) = frequency of (x, y) / frequency of total keyword.
                 # p(x) = frequency of x / frequency of total keyword.
-                print(kcm_pair)
                 value=(math.log10( int(kcm_pair[1]) * 154451970  /(float(keyword_freq) * int(self.search_word_freq(kcm_pair[0]))  )) / math.log10(2))
 
                 # this equation is contributed by 陳聖軒. 
@@ -80,6 +77,5 @@ class PMI(object):
 
 if __name__ == '__main__':
     p = PMI()
-    # p.build()
+    p.build()
     print(p.get('蔡英文', 10))
-    
