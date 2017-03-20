36d077d7abd526768a7f509ac559ea5b3b1ebc12
davidtnfsh <davidtnfsh@gmail.com>
2017-03-20 20:29:52 +0800

    turn it into django app + add README.md

--- PMIofKCM/__init__.py
+++ PMIofKCM/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import math, pyprind, json
+import math
 from pymongo import MongoClient
 
 class PMI(object):
--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ from distutils.core import setup
 setup(
     name = 'PMIofKCM',
     packages = ['PMIofKCM'],
-    version = '1.0',
+    version = '1.1',
     description = 'PMIofKCM for KCM',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/UDICatNCHU/PMIofKCM',
-    download_url = 'https://github.com/UDICatNCHU/PMIofKCM/archive/v1.0.tar.gz',
-    keywords = ['kcm',],
+    download_url = 'https://github.com/UDICatNCHU/PMIofKCM/archive/v1.1.tar.gz',
+    keywords = ['pmi',],
     classifiers = [],
     license='GPL3.0',
     install_requires=[
-        'pyprind',
-        'simplejson',
         'pymongo'
     ],
     zip_safe=True
